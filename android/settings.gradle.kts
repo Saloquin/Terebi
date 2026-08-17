7441970ccc830afbbac098c2d7b2900235a5d4cd
I763888 <thomas.roger@sap.com>
2026-08-17 12:21:41 +0200

    fix(android): override wakelock_plus 1.7.0 (Kotlin 2.1 compat) + AGP 8.7/Kotlin 2.1, build APK OK

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -19,8 +19,9 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    // AGP 8.7 + Kotlin 2.1 : versions stables compatibles avec le plugin Flutter
-    // 3.41 (AGP 9 casse l'application du plugin Flutter : NullPointerException).
+    // AGP 8.7 + Kotlin 2.1 : requis par package_info_plus 9. wakelock_plus est
+    // force a 1.7.0 (dependency_overrides) car <=1.5.2 casse avec Kotlin 2.1
+    // (import du package racine). Compatible plugin Flutter 3.41.
     id("com.android.application") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
